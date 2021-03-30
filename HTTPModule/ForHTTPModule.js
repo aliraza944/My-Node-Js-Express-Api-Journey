a56bf56ca9fa402eddd84b0dce28050e526410dd
Ali Raza <alisabri022@gmail.com>
2021-03-30 20:24:36 +0500

    HTTP module

---
+++ HTTPModule/ForHTTPModule.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const readStream = fs.createReadStream("../static/example.json");
+  res.writeHead(200, { "Content-type": "application/json" });
+
+  readStream.pipe(res);
+}); //end serverb
+
+server.listen("3000");
